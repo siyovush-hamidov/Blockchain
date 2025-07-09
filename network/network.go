2a358dbc5d957b4ed3d1e15e5ae5a94b2fb91a95
siyovush-hamidov <siyovushchik14@gmail.com>
2025-07-10 03:52:41 +0500

    +функции: SerializePackage, readPackage, DeserializePackage + consts

--- network/network.go
+++ network/network.go
@@ -1,7 +1,9 @@
 package network
 
 import (
+	"encoding/json"
 	"net"
+	"strings"
 	"time"
 )
 
@@ -10,6 +12,16 @@ type Package struct
 	Option int
 	Data string
 }
+
+const (
+	ENDBYTES = "\000\005\007\001\001\007\005\000"
+)
+
+const(
+	WAITTIME = 5 // seconds
+	DMAXSIZE = (2 << 20) // 2^20*2 = 2MiB
+	BUFFSIZE = 4 << 10 // 2^10*4 = 4KiB
+)
 
 func Send(address string, pack *Package) *Package {
 	conn, err := net.Dial("tcp", address)
@@ -28,4 +40,45 @@ func Send(address string, pack *Package) *Package {
 		case <-time.After(WAITTIME * time.Second):
 	}
 	return res
+}
+
+func SerializePackage(pack *Package) string{
+	jsonData, err := json.MarshalIndent(*pack, "", "\t")
+	if err != nil{
+		return ""
+	}
+	return string(jsonData)
+}
+
+func readPackage(conn net.Conn) *Package {
+	var(
+		data string
+		size = uint64(0)
+		buffer = make([]byte, BUFFSIZE)
+	)
+	for {
+		length, err := conn.Read(buffer)
+		if err != nil{
+			return nil
+		}
+		size += uint64(length)
+		if size > DMAXSIZE{
+			return nil
+		}
+		data += string(buffer[:length])
+		if strings.Contains(data, ENDBYTES) {
+			data = strings.Split(data, ENDBYTES)[0]
+			break
+		}
+	}
+	return DeserializePackage(data)
+}
+
+func DeserializePackage(data string) *Package{
+	var pack Package
+	err := json.Unmarshal([]byte(data), &pack)
+	if err != nil{
+		return nil
+	}
+	return &pack
 }
